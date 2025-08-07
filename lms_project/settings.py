b3885716a4b47876b12d05909478415ac945f879
Krishna Pathak <kriishna.pathak@gmail.com>
2025-08-07 11:30:18 +0545

    updated

--- lms_project/settings.py
+++ lms_project/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
+import os
+from dotenv import load_dotenv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,10 +22,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-q8wxbqqvr=y(2l4y&%xtafg7jhe*y1u5jm0_1mc=m+75m5k+%9'
-
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+load_dotenv()  # Load .env file
+SECRET_KEY = os.environ.get('SECRET_KEY') 
+DEBUG = os.environ.get('DEBUG') == 'True'
 
 ALLOWED_HOSTS = []
 
